9e0e9a1054bd44ba261338af60e9d8a3c424bf0e
jwklong <72522395+jwklong@users.noreply.github.com>
2025-11-19 22:19:30 +0000

    add cyan theme

---
+++ src/lib/themes/accent/cyan.js
@@ -0,0 +1,13 @@
+const guiColors = {
+    'looks-secondary': 'hsla(194, 100%, 65%, 1)',
+    'looks-transparent': 'hsla(194, 100%, 65%, 0.35)',
+    'looks-light-transparent': 'hsla(194, 100%, 65%, 0.15)',
+    'looks-secondary-dark': 'hsla(194, 60%, 50%, 1)'
+};
+
+const blockColors = {};
+
+export {
+    guiColors,
+    blockColors
+};
--- src/lib/themes/index.js
+++ src/lib/themes/index.js
@@ -3,6 +3,7 @@ import defaultsDeep from 'lodash.defaultsdeep';
 import * as accentPurple from './accent/purple';
 import * as accentBlue from './accent/blue';
 import * as accentRed from './accent/red';
+import * as accentCyan from './accent/cyan';
 import * as accentRainbow from './accent/rainbow';
 
 import * as guiLight from './gui/light';
@@ -15,14 +16,16 @@ import * as blocksDark from './blocks/dark';
 const ACCENT_PURPLE = 'purple';
 const ACCENT_BLUE = 'blue';
 const ACCENT_RED = 'red';
+const ACCENT_CYAN = 'cyan';
 const ACCENT_RAINBOW = 'rainbow';
 const ACCENT_MAP = {
     [ACCENT_PURPLE]: accentPurple,
     [ACCENT_BLUE]: accentBlue,
     [ACCENT_RED]: accentRed,
+    [ACCENT_CYAN]: accentCyan,
     [ACCENT_RAINBOW]: accentRainbow
 };
-const ACCENT_DEFAULT = ACCENT_RED;
+const ACCENT_DEFAULT = ACCENT_CYAN;
 
 const GUI_LIGHT = 'light';
 const GUI_DARK = 'dark';
@@ -149,6 +152,7 @@ export {
     ACCENT_RED,
     ACCENT_PURPLE,
     ACCENT_BLUE,
+    ACCENT_CYAN,
     ACCENT_RAINBOW,
     ACCENT_MAP,
 
