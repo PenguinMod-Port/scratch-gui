8bd376e01893d6752c36a7ff4ac3cea7e20ef9c6
jwklong <72522395+jwklong@users.noreply.github.com>
2025-11-21 18:26:04 +0000

    improve accent

--- src/lib/themes/accent/cyan.js
+++ src/lib/themes/accent/cyan.js
@@ -1,11 +1,23 @@
 const guiColors = {
+    'motion-primary': 'hsla(194, 100%, 65%, 1)',
+    'motion-primary-transparent': 'hsla(194, 100%, 65%, 0.9)',
+    'motion-tertiary': 'hsla(194, 100%, 40%, 1)',
+
     'looks-secondary': 'hsla(194, 100%, 65%, 1)',
     'looks-transparent': 'hsla(194, 100%, 65%, 0.35)',
     'looks-light-transparent': 'hsla(194, 100%, 65%, 0.15)',
-    'looks-secondary-dark': 'hsla(194, 100%, 40%, 1)'
+    'looks-secondary-dark': 'hsla(194, 100%, 40%, 1)',
+    
+    'extensions-primary': 'hsla(205, 100%, 65%, 1)',
+    'extensions-tertiary': 'hsla(205, 100%, 40%, 1)',
+    'extensions-transparent': 'hsla(205, 100%, 65%, 0.35)',
+    'extensions-light': 'hsla(205, 60%, 85%, 1)',
 };
 
-const blockColors = {};
+const blockColors = {
+    checkboxActiveBackground: '#4dd5ff',
+    checkboxActiveBorder: '#009ccc'
+};
 
 export {
     guiColors,
