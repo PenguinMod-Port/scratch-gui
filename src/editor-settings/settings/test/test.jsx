564d4ae32f82b92604e738f51ac6016004dc1413
jwklong <jwklong@gmx.com>
2026-08-11 14:50:30 +0100

    bumpscosity setting
    Signed-off-by: jwklong <jwklong@gmx.com>

--- src/editor-settings/settings/test/test.jsx
+++ src/editor-settings/settings/test/test.jsx
@@ -1,6 +1,23 @@
-import BooleanSetting from "../../components/boolean-setting/boolean-setting.jsx";
+import IntegerSetting from "../../components/integer-setting/integer-setting.jsx";
+import {FormattedMessage} from 'react-intl';
 import React from 'react';
 
-export default (class extends BooleanSetting {
-    
+export default (class extends IntegerSetting {
+    defaultValue() { return 100 }
+    min = 0
+    max = 1000
+
+    getPrimary() {
+        return (<FormattedMessage
+            defaultMessage="Bumpscosity"
+            id="pm.editorSettings.test.primary"
+        />)
+    }
+
+    getHelp() {
+        return (<FormattedMessage
+            defaultMessage="Changes the level of bumpscosity."
+            id="pm.editorSettings.test.help"
+        />)
+    }
 });
