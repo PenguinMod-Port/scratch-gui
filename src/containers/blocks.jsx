eb5879ba58d925440dc56100874e553e2b8c40e9
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2026-09-01 15:46:37 -0700

    I hate react

--- src/containers/blocks.jsx
+++ src/containers/blocks.jsx
@@ -900,8 +900,8 @@ const mapStateToProps = state => ({
     locale: state.locales.locale,
     messages: state.locales.messages,
     toolboxXML: state.scratchGui.toolbox.toolboxXML,
-    commentColorEditorVisible: state.scratchGui.commentColorEditorVisible.active,
-    commentFontEditorVisible: state.scratchGui.commentFontEditorVisible.active,
+    commentColorEditorVisible: state.scratchGui.commentEditor.active,
+    commentFontEditorVisible: state.scratchGui.commentEditor.active,
     customProceduresVisible: state.scratchGui.customProcedures.active,
     workspaceMetrics: state.scratchGui.workspaceMetrics,
     useCatBlocks: isTimeTravel2020(state)
