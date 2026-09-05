0c33413e35d32a4dc869f9e367415c1bf91704ed
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2026-09-04 23:52:45 -0700

    Forcfully update toolbox with new editor setting

--- src/editor-settings/settings/blockCounter/blockCounter.jsx
+++ src/editor-settings/settings/blockCounter/blockCounter.jsx
@@ -2,10 +2,9 @@ import BooleanSetting from "../../components/boolean-setting/boolean-setting.jsx
 import {FormattedMessage} from 'react-intl';
 import React from 'react';
 import LazyScratchBlocks from "../../../lib/tw-lazy-scratch-blocks.js";
-import refreshWorkspace from "../../util/refreshWorkspace.js";
 
 export default (class extends BooleanSetting {
-    defaultValue() { return true }
+    defaultValue() { return false }
 
     getPrimary() {
         return (<FormattedMessage
@@ -26,6 +25,16 @@ export default (class extends BooleanSetting {
         let ScratchBlocks = LazyScratchBlocks.get();
 
         ScratchBlocks.Toolbox.Category.SHOW_BLOCK_COUNT = value;
+
+        // Manually update toolbox categories
+        const toolbox = ScratchBlocks.getMainWorkspace().getToolbox();
+        const categories = toolbox.categoryMenu_.categories_;
+        if (value) {
+            for (const category of categories) category.createCounter(true);
+        } else {
+            for (const category of categories) category.removeCounter();
+        }
+
         refreshWorkspace(ScratchBlocks);
     }
 });
