b412c355e64f8c12f788200b40c597186e893c9d
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2026-09-08 01:19:58 -0700

    potential fix

--- src/components/sprite-selector-item/sprite-selector-item.jsx
+++ src/components/sprite-selector-item/sprite-selector-item.jsx
@@ -77,7 +77,7 @@ const SpriteSelectorItem = props => (
                     </MenuItem>
                 ) : null }
                 {props.onSpecialExportButtonClick ? (
-                    <MenuItem onClick={props.onSpecialExportButtonClick}>
+                    <MenuItem onClick={() => props.onSpecialExportButtonClick(props.index)}>
                         <FormattedMessage
                             defaultMessage="export with extras"
                             description="Menu item to export the selected item with extra features"
@@ -113,6 +113,7 @@ SpriteSelectorItem.propTypes = {
     componentRef: PropTypes.func,
     costumeURL: PropTypes.string,
     details: PropTypes.string,
+    index: PropTypes.number,
     // eslint-disable-next-line react/forbid-prop-types
     name: PropTypes.any,
     number: PropTypes.number,
