197fddda89b1d7d19edf5b4d8335b2e5ee32f9cd
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2026-09-08 00:39:34 -0700

    GUI -- Ability to export costumes with custom fonts & Better settings css

--- src/components/asset-panel/selector.jsx
+++ src/components/asset-panel/selector.jsx
@@ -26,6 +26,7 @@ const Selector = props => {
         onDeleteClick,
         onDuplicateClick,
         onExportClick,
+        onSpecialExportClick,
         onItemClick
     } = props;
 
@@ -81,6 +82,7 @@ const Selector = props => {
                             onDeleteButtonClick={onDeleteClick}
                             onDuplicateButtonClick={onDuplicateClick}
                             onExportButtonClick={onExportClick}
+                            onSpecialExportButtonClick={onSpecialExportClick}
                         />
                     </SortableAsset>
                 ))}
--- src/containers/costume-tab.jsx
+++ src/containers/costume-tab.jsx
@@ -79,6 +79,7 @@ class CostumeTab extends React.Component {
             'handleDeleteCostume',
             'handleDuplicateCostume',
             'handleExportCostume',
+            'handleExportCostumeExtras',
             'handleNewCostume',
             'handleNewBlankCostume',
             'handleSurpriseCostume',
@@ -146,6 +147,13 @@ class CostumeTab extends React.Component {
         const item = this.props.vm.editingTarget.sprite.costumes[costumeIndex];
         const blob = new Blob([
             this.props.vm.getExportedCostume(item)
+        ], {type: item.asset.assetType.contentType});
+        downloadBlob(`${item.name}.${item.asset.dataFormat}`, blob);
+    }
+    handleExportCostumeExtras (costumeIndex) {
+        const item = this.props.vm.editingTarget.sprite.costumes[costumeIndex];
+        const blob = new Blob([
+            this.props.vm.getExportedCostume(item, true, this.props.vm)
         ], {type: item.asset.assetType.contentType});
         downloadBlob(`${item.name}.${item.asset.dataFormat}`, blob);
     }
@@ -315,6 +323,7 @@ class CostumeTab extends React.Component {
                 onDrop={this.handleDrop}
                 onDuplicateClick={this.handleDuplicateCostume}
                 onExportClick={this.handleExportCostume}
+                onSpecialExportClick={this.handleExportCostumeExtras}
                 onItemClick={this.handleSelectCostume}
             >
                 {target.costumes ?
